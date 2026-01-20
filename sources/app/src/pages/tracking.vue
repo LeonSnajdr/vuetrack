67f04c14d561b2eb1a1cfb34bc62f9ba065a6041
LeonSnajdr <94352142+LeonSnajdr@users.noreply.github.com>
2026-01-20 19:20:18 +0100

    Remove test functions

--- sources/app/src/pages/tracking.vue
+++ sources/app/src/pages/tracking.vue
@@ -1,21 +1,8 @@
 <template>
     <TrackingCalendarSidebar />
-    <VBtn @click="timeEntryStore.add()">{{ $t("action.add") }}</VBtn>
-    <VBtn @click="timeEntryStore.removeLast()" :disabled="timeEntries.length === 0" class="ml-2" color="error" variant="outlined">{{
-        $t("tracking.removeLast")
-    }}</VBtn>
-    <VBtn @click="timeEntryStore.addOneHour()">{{ $t("tracking.addOneHour") }}</VBtn>
-    <template v-if="timeEntries[0]">
-        <p>{{ timeEntries[0].startTime }} /{{ timeEntries[0].endTime }}</p>
-        <VTextField v-model="timeEntries[0].taskId" />
-    </template>
     <VContainer class="h-100">
         <TrackingCalendarView />
     </VContainer>
 </template>
 
-<script setup lang="ts">
-const timeEntryStore = useTimeEntryStore();
-
-const { timeEntries } = storeToRefs(timeEntryStore);
-</script>
+<script setup lang="ts"></script>
--- sources/app/src/stores/timeEntryStore.ts
+++ sources/app/src/stores/timeEntryStore.ts
@@ -40,26 +40,5 @@ export const useTimeEntryStore = defineStore("timeEntry", () => {
         return deleteResult;
     };
 
-    const add = () => {
-        timeEntries.value.push({
-            id: "myId" as TimeEntryId,
-            user: "leon",
-            startTime: new Date(),
-            endTime: new Date(Date.now() + 60 * 60 * 1000),
-            taskId: "myTask"
-        });
-    };
-
-    const removeLast = () => {
-        timeEntries.value.pop();
-    };
-
-    const addOneHour = () => {
-        const entry = timeEntries.value[0];
-        if (!entry) return;
-
-        entry.endTime = new Date(entry.endTime.getTime() + 60 * 60 * 1000);
-    };
-
-    return { timeEntries, add, removeLast, addOneHour, create, update, remove, cancelPendingUpdate };
+    return { timeEntries, create, update, remove, cancelPendingUpdate };
 });
