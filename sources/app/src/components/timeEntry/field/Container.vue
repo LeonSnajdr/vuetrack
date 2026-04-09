dea02654e0dc77f969ef320089a3faa374cdf1f0
LeonSnajdr <mail@leonsnajdr.de>
2026-04-09 20:57:10 +0200

    Autofocus firstEmptyField

--- sources/app/src/components/timeEntry/field/Container.vue
+++ sources/app/src/components/timeEntry/field/Container.vue
@@ -1,11 +1,16 @@
 <template>
-    <VForm v-model="valid">
-        <TimeEntryFieldTaskId v-model="timeEntry.taskId" autofocus />
-        <TimeEntryFieldStartTime v-model="timeEntry.startTime" :tabindex="skipTimeFields ? -1 : undefined" />
-        <TimeEntryFieldEndTime v-model="timeEntry.endTime" :startTime="timeEntry.startTime" :tabindex="skipTimeFields ? -1 : undefined" />
-        <TimeEntryFieldProjectId v-model="timeEntry.projectId" :taskId="timeEntry.taskId" />
-        <TimeEntryFieldActivityId v-model="timeEntry.activityId" :projectId="timeEntry.projectId" />
-        <TimeEntryFieldComment v-model="timeEntry.comment" />
+    <VForm :key="reFocusKey" v-model="valid">
+        <TimeEntryFieldTaskId v-model="timeEntry.taskId" :autofocus="firstEmptyField === 'taskId'" />
+        <TimeEntryFieldStartTime v-model="timeEntry.startTime" :autofocus="firstEmptyField === 'startTime'" :tabindex="skipTimeFields ? -1 : undefined" />
+        <TimeEntryFieldEndTime
+            v-model="timeEntry.endTime"
+            :autofocus="firstEmptyField === 'endTime'"
+            :startTime="timeEntry.startTime"
+            :tabindex="skipTimeFields ? -1 : undefined"
+        />
+        <TimeEntryFieldProjectId v-model="timeEntry.projectId" :autofocus="firstEmptyField === 'projectId'" :taskId="timeEntry.taskId" />
+        <TimeEntryFieldActivityId v-model="timeEntry.activityId" :autofocus="firstEmptyField === 'activityId'" :projectId="timeEntry.projectId" />
+        <TimeEntryFieldComment v-model="timeEntry.comment" :autofocus="firstEmptyField === 'comment'" />
     </VForm>
 </template>
 
@@ -13,7 +18,7 @@
 import type { ActivityId } from "@/contracts/ActivityContract";
 import type { ProjectId } from "@/contracts/ProjectContract";
 
-defineProps<{
+const props = defineProps<{
     skipTimeFields?: boolean;
 }>();
 
@@ -27,4 +32,19 @@ const timeEntry = defineModel<{
 }>({ required: true });
 
 const valid = defineModel<boolean>("valid", { default: false });
+
+const firstEmptyField = computed(() => {
+    if (!timeEntry.value.taskId) return "taskId";
+    if (!props.skipTimeFields && !timeEntry.value.startTime) return "startTime";
+    if (!props.skipTimeFields && !timeEntry.value.endTime) return "endTime";
+    if (!timeEntry.value.projectId) return "projectId";
+    if (!timeEntry.value.activityId) return "activityId";
+    if (!timeEntry.value.comment) return "comment";
+
+    return null;
+});
+
+// If the time entry is completely reset (which could happen trough create another), the form is rerenderd to focus again the correct field
+const reFocusKey = ref(crypto.randomUUID());
+watch(timeEntry, () => (reFocusKey.value = crypto.randomUUID()));
 </script>
